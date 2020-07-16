3fd2c3c822c7827325a0e4947b1d941471e51959
Oliver Louis <oliverlouis@me.com>
2020-07-16 22:24:08 +0200

    fix view projects responsiveness

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const photon = document.querySelector('.photon');
 const forkify = document.querySelector('.forkify');
 const rawr = document.querySelector('.rawr');
 
+const viewWorkImagePhoton = document.querySelector('.view-work-img-photon');
+const viewWorkImageForkify = document.querySelector('.view-work-img-forkify');
+const viewWorkImageRawr = document.querySelector('.view-work-img-rawr');
+
 const photonDesc = document.querySelector('.photon-desc');
 const forkifyDesc = document.querySelector('.forkify-desc');
 const rawrDesc = document.querySelector('.rawr-desc');
@@ -10,26 +14,32 @@ const rawrDesc = document.querySelector('.rawr-desc');
 
 photon.addEventListener('mouseover', function () {
 	photonDesc.style.display = 'block';
+	viewWorkImagePhoton.style.opacity = '0.4';
 });
 
 forkify.addEventListener('mouseover', function () {
 	forkifyDesc.style.display = 'block';
+	viewWorkImageForkify.style.opacity = '0.4';
 });
 
 rawr.addEventListener('mouseover', function () {
 	rawrDesc.style.display = 'block';
+	viewWorkImageRawr.style.opacity = '0.4';
 });
 
 //MOUSEOUT
 
 photon.addEventListener('mouseout', function () {
 	photonDesc.style.display = 'none';
+	viewWorkImagePhoton.style.opacity = '1';
 });
 
 forkify.addEventListener('mouseout', function () {
 	forkifyDesc.style.display = 'none';
+	viewWorkImageForkify.style.opacity = '1';
 });
 
 rawr.addEventListener('mouseout', function () {
 	rawrDesc.style.display = 'none';
+	viewWorkImageRawr.style.opacity = '1';
 });
